a61ce6c4ff7e9f3b0bfcf997327a4d86b33b3b6b
root <root@philhansen2.mylabserver.com>
2018-11-16 14:51:22 +0000

    test site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,7 +12,6 @@
 ## Active Configurations ##
 
 # Disable filebucket by default for all File resources:
-#https://docs.puppet.com/pe/2015.3/release_notes.html#filebucket-resource-no-longer-created-by-default
 File { backup => false }
 
 # DEFAULT NODE
@@ -25,8 +24,21 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+node 'philhansen3.mylabserver' {
+  notify { 'This matches nname': }
+}
+
+node /^philhansen3/ {
+  notify { "This is a node def for regx": }
+}
+
+node 'philhansen2.mylabserver.com' {
+  include pe_repo::platform::el_6_x86_64
+  include pe_repo::platform::ubuntu_1204_amd64
+}
+
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
+  notify { "This is the default node": }
+  class { 'java': }
+#  class { 'ntp': }
 }
